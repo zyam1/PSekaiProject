6f2dba0809e2835b854d3789665a41ecd00ec972
unknown <zyam8441@gmail.com>
2024-03-06 21:53:45 +0900

    주석추가

--- Assets/Scripts/SceneChanger/LoadtoMakedata.cs
+++ Assets/Scripts/SceneChanger/LoadtoMakedata.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//씬 너무 많이 나누나???
 public class LoadtoMakedata : MonoBehaviour
 {
     // Start is called before the first frame update
